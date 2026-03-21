d1ea98575648196d4ede68c425579ba7e309aac2
barrier <barrier0630.15300@gmail.com>
2026-03-21 14:25:13 +0900

    Revert "レビューを受けた修正"
    This reverts commit 05ab869a69263d4d4b1d7fba85479cf41a3ee459.

--- include/libarrier/ringbuffer.hpp
+++ include/libarrier/ringbuffer.hpp
@@ -145,12 +145,7 @@ private:
 	constexpr void write_back_impl(fT&& v) noexcept {
 		(*this)[m_current_size] = std::forward<fT>(v);
 
-		if (++m_current_size >= buffer_size()) {
-			m_current_size = buffer_size() - 1;
-		} else {
-			return;
-		}
-			if (!(++m_current_size < size())) {
+			if (++m_current_size >= size()) {
 				m_current_size = size() - 1;
 			}
 			else {
