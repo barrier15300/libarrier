e7b5605475fab34191f8b30cfe56c39d903b62f3
barrier <barrier0630.15300@gmail.com>
2026-03-21 16:30:13 +0900

    雛形を追加

---
+++ include/libarrier/task.hpp
@@ -0,0 +1,17 @@
+#ifndef LIBARRIER_TASK_HPP
+#define LIBARRIER_TASK_HPP
+
+#include "functional.hpp"
+
+#include <thread>
+
+template<typename T>
+class Task {
+	std::thread m_thread;
+	bool m_finished = false;
+
+public:
+
+};
+
+#endif // LIBARRIER_TASK_HPP
