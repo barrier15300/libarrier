e5d5d9e8090d8899b9e706507a060680130239b6
barrier <barrier0630.15300@gmail.com>
2026-03-21 18:48:36 +0900

    std::async関数のラッパー、Task<T>の実装

--- include/libarrier/task.hpp
+++ include/libarrier/task.hpp
@@ -1,17 +1,43 @@
 #ifndef LIBARRIER_TASK_HPP
 #define LIBARRIER_TASK_HPP
 
-#include "functional.hpp"
-
+#include <chrono>
+#include <future>
+#include <optional>
 #include <thread>
+
+namespace libarrier {
 
 template<typename T>
 class Task {
-	std::thread m_thread;
-	bool m_finished = false;
+	std::future<T> m_future;
 
 public:
 
+	template<typename F, typename... Args>
+	Task(F&& proc, Args&&... args) {
+		m_future = std::async(std::launch::async, proc, std::forward<Args>(args)...);
+	}
+
+	bool IsFinished() const {
+		return m_future.wait_for(std::chrono::seconds::zero()) == std::future_status::ready;
+	}
+
+	void Wait() {
+		while (!IsFinished()) {
+			std::this_thread::yield();
+		}
+	}
+
+	std::optional<T> ResultAsync() const {
+		return IsFinished() ? m_future.get() : std::nullopt;
+	}
+	T Result() {
+		Wait();
+		return m_future.get();
+	}
 };
+
+} // namespace libarrier
 
 #endif // LIBARRIER_TASK_HPP
--- libarrier.h
+++ libarrier.h
@@ -1,6 +1,12 @@
 ﻿#pragma once
 
+//#define SINGLE_TEST
+
+#ifdef SINGLE_TEST
 #include "include/libarrier/arrier.hpp"
+#else
+#include "include/libarrier/task.hpp"
+#endif
 
+#include <cstdio>
 #include <iostream>
-
