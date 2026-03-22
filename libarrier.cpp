782f8d601ef87e23a0367b13f036743d3e98251b
barrier <barrier0630.15300@gmail.com>
2026-03-22 14:55:53 +0900

    競合解決

--- libarrier.cpp
+++ libarrier.cpp
@@ -1,56 +1,14 @@
 #include "libarrier.h"
-#include <cstdio>
 
-struct Functor {
-	int v = 3;
-	void operator()(int a) const {
-		printf("Functor: %d\n", v * a);
-	}
-};
-
-class Hoge {
-public:
-
-	void mut(int a) {
-		printf("Hoge::mut: %d\n", a);
-	}
-	void imut(int a) const {
-		printf("Hoge::imut: %d\n", a);
-	}
-};
-
-void func(int a) {
-	printf("func: %d\n", a);
-}
+#include "include/libarrier/functional.hpp"
+#include "include/libarrier/task.hpp"
 
 int main() {
 	using namespace libarrier;
 
-	int b = 16;
-
-	function f1 = func;
-	function f2 = [](int a) {
-		printf("lambda: %d\n", a);
-	};
-	function f3 = [b](int a) {
-		printf("captured lambda: %d\n", b * a);
-	};
-
-	Hoge obj;
-	Functor functor;
-
-	function f4 = {obj, &Hoge::mut};
-	function f5 = {obj, &Hoge::imut};
-	function f6 = functor;
-	function f7 = Functor(10);
-
-	f1(1);
-	f2(2);
-	f3(3);
-	f4(4);
-	f5(5);
-	f6(6);
-	f7(7);
+	Task task = Task<int>([&]() {
+		return 0;
+	});
 
 	return 0;
 }
