8e7008cf2e22fb7f8fdfd47ff380b62616859ba3
barrier <barrier0630.15300@gmail.com>
2026-04-18 12:16:19 +0900

    wipなhppをコメントアウト

--- include/libarrier/arrier.hpp
+++ include/libarrier/arrier.hpp
@@ -1,13 +1,12 @@
 #ifndef LIBARRIER_ARRIER_HPP
 #define LIBARRIER_ARRIER_HPP
 
-#include "binarydata.hpp"
+//#include "binarydata.hpp"
 #include "functional.hpp"
-#include "match.hpp"
+//#include "match.hpp"
 #include "ringbuffer.hpp"
 #include "task.hpp"
 #include "textfile_reader.hpp"
 #include "timer.hpp"
-
 
 #endif // LIBARRIER_ARRIER_HPP
