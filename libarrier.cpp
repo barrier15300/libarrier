381c24b96dd225b30db473be73b0ef9d365b3108
barrier <barrier0630.15300@gmail.com>
2026-08-13 00:15:19 +0900

    TextFileReaderの小修正

--- libarrier.cpp
+++ libarrier.cpp
@@ -1,16 +1,20 @@
 #include "libarrier.h"
 
-#include "include/libarrier/binarydata.hpp"
+#include "include/libarrier/textfile_reader.hpp"
 //#include <map>
 #include <random>
 #include <format>
 
 using namespace libarrier;
 
+int main(int argc, char** argv) {
 
-int main(int argc, char **argv) {
 
-	
+	for (auto&& entry : std::filesystem::directory_iterator("S:\\MyMakingProject\\NowCoding\\libarrier\\out\\build\\x64-debug\\test")) {
+		auto&& p = entry.path();
+		TextfileReader reader(p);
+	}
 
-  return 0;
+
+	return 0;
 }
